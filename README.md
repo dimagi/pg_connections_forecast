9a977109b7cd810266f8ca4e8221df78688cfa7e
Simon Kelly <skelly@dimagi.com>
2017-03-20 12:16:39 +0200

    tweaks

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ $ pip install -r requirements.txt
 # Pull last 30 days of data
 $ python pull_postgresl_data.py --api_key=<datadog api key> --app_key=<datadog app key> --days-past=30 > postgres_connections.csv
 
+# Pull all data since last pull and append it to the current data file
+$ tail -n 1 postgres_connections.csv
+2017-03-20 09:30:00,53.1428563595
+
+$ python pull_postgresl_data.py --api_key=<datadog api key> --app_key=<datadog app key> --since=2017-03-20T09:30:00 --append >> postgres_connections.csv
+
 # Run notebook server and open the postgresql_connections.ipynb notebook.
 $ jupyter notebook
 ```
--- pull_postgresl_data.py
+++ pull_postgresl_data.py
@@ -1,22 +1,46 @@
 import argparse
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from datadog import initialize, api
 
 
+def parse_date(iso_string):
+    try:
+        return datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%S')
+    except ValueError:
+        pass
+
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("api_key",
+    parser.add_argument("--api_key",
         help="Datadog API Key.")
-    parser.add_argument("app_key",
+    parser.add_argument("--app_key",
         help="Datadog App Key")
-    parser.add_argument("--days-past", default=30,
-        help="How many days of data to fetch")
+    parser.add_argument("--days-past", default=-1,
+        help="How many days of data to fetch (measured from now).")
+    parser.add_argument("--since", default=None,
+                        help="Fetch data since this date (UTC).")
+    parser.add_argument("--append", action='store_true',
+                        help="Don't print the headers.")
 
     args = parser.parse_args()
 
     days_past = int(args.days_past)
+    since = None
+
+    if days_past > 0 and args.since:
+        print "Only one of --days-past and --since allowed, not both."
+
+    if args.since:
+        since = parse_date(args.since)
+        if not since:
+            print "Unable to parse date: {}. Expected format: 'YYYY-MM-DDThh:mm:ss'".format(since)
+            return
+
+    if days_past < 0 and not since:
+        print "One of --since and --days-past required"
 
     options = {
         'api_key': args.api_key,
@@ -25,17 +49,25 @@ def main():
 
     initialize(**options)
 
-    now = int(time.time())
+    if not since:
+        since = datetime.utcnow() - timedelta(days=days_past)
+
+    since_epoch = int(since.strftime("%s"))
+    now_epoch = int(datetime.utcnow().strftime("%s"))
     query = 'sum:postgresql.connections{environment:production,host:hqdb0}'
     seconds_per_day = 3600 * 24
 
-    print "Datetime,Connections"
-    for day in range(-days_past, 0):
-        start = now + (day * seconds_per_day)
-        end = now + ((day + 1) * seconds_per_day)
+    if not args.append:
+        print "Datetime,Connections"
+
+    while since_epoch < now_epoch:
+        start = since_epoch
+        end = since_epoch + seconds_per_day
         response = api.Metric.query(start=start, end=end, query=query)
         for point in response['series'][0]['pointlist']:
-            print '{},{}'.format(datetime.utcfromtimestamp(int(point[0]/1000)), point[1])
+            print '{},{}'.format(datetime.fromtimestamp(int(point[0] / 1000)), point[1])
+
+        since_epoch = end
 
 if __name__ == "__main__":
     main()
